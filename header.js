1f834db23647f6f2e14f4c3a12367bd356e2243a
William chitapa <williamchitapa@gmail.com>
2025-03-09 11:45:03 -0700

    WorkanaWebsite

--- header.js
+++ header.js
@@ -1,10 +1,9 @@
 const menuToggle = document.getElementById('menuToggle');
 const navLinks = document.getElementById('navLinks');
 
-// Alterna o menu entre visível/invisível
 menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active'); // Animação do hambúrguer para "X"
-    navLinks.classList.toggle('show');    // Mostra ou esconde o menu
+    menuToggle.classList.toggle('active'); 
+    navLinks.classList.toggle('show');    
 });
 
 
